80f7f3ef37eb2eda4a33e74c4e8aac9121c64169
Maria Violaki <mariaviolaki@outlook.com>
2025-01-06 16:29:14 +0200

    feat(dialogue): typewriter and instant mode

---
+++ Assets/_Main/Scripts/DialogueUI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueUI : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI nameText;
+	[SerializeField] TextMeshProUGUI dialogueText;
+
+	TextBuilder textBuilder;
+
+	string[] testStrings =
+	{
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+		"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+		"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	};
+
+    void Awake()
+    {
+		textBuilder = new TextBuilder(dialogueText, 6);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+			DisplayText();
+		}
+    }
+
+	void DisplayText()
+	{
+		string text = testStrings[Random.Range(0, testStrings.Length - 1)];
+		textBuilder.Build(text);
+	}
+}
---
+++ Assets/_Main/Scripts/TextBuilder.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class TextBuilder
+{
+	const int minSpeed = 1;
+	const int maxSpeed = 10; // instant
+
+	Coroutine buildProcess;
+	TMP_Text tmpText;
+	int speed;
+
+	public int Speed { get { return speed; } set { speed = Mathf.Clamp(value, minSpeed, maxSpeed); } }
+
+	public TextBuilder(TMP_Text tmpText, int speed)
+	{
+		this.tmpText = tmpText;
+		this.speed = Mathf.Clamp(speed, minSpeed, maxSpeed);
+	}
+
+	public void Build(string text)
+	{
+		bool isBuilding = buildProcess != null;
+		if (isBuilding)
+		{
+			CompleteProcess();
+		}
+
+		// If the player interacts before the text is completed, complete it immediately
+		int textSpeed = isBuilding ? maxSpeed : speed;
+
+		buildProcess = tmpText.StartCoroutine(BuildText(text, textSpeed));
+	}
+
+	IEnumerator BuildText(string text, int textSpeed)
+	{
+		int textCount = text.Count();
+
+		tmpText.text = text;
+		tmpText.maxVisibleCharacters = 0;
+		tmpText.ForceMeshUpdate();
+
+		while (tmpText.maxVisibleCharacters < textCount)
+		{
+			// Skip some frames for speed 1-5
+			int iterations = textSpeed;
+			while (iterations < maxSpeed)
+			{
+				iterations += textSpeed;
+				yield return null;
+			}
+
+			// After some frames are skipped or when speed is 6-10
+			// Immediately reveal 1-5 characters or the entire string
+			tmpText.maxVisibleCharacters += GetCharactersPerIteration(textCount, textSpeed);
+			tmpText.ForceMeshUpdate();
+			yield return null;
+		}
+
+		CompleteProcess();
+	}
+
+	void CompleteProcess()
+	{
+		tmpText.StopCoroutine(buildProcess);
+		buildProcess = null;
+	}
+
+	int GetCharactersPerIteration(int textCount, int textSpeed)
+	{
+		// If speed is 10, all characters are revealed instantly
+		if (textSpeed == maxSpeed) return textCount;
+		// For speed 1-5, 1 character is revealed
+		else if (textSpeed < 6) return 1;
+		// For speed 6-9, 2-5 characters are revealed
+		else return textSpeed - 4;
+	}
+}
